c414399bcd2a5a99827f46c544214378e9c106b3
Breno Vergilio <bjvergilio47@gmail.com>
2021-04-22 23:17:07 -0400

    Adiciona tela de ediçao de conteudo

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home', to: "home#index"
-  get 'contents', to:"contents#index"
-  get 'contents/new', to:"contents#new"
-  get 'tags', to:"tags#index"
 
+  resources :contents
 end
